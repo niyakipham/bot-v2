41466573dbb671b165692b7ac8268c40f0adc6e6
Niyaki Pham <niyakipham@gmail.com>
2024-10-14 16:17:56 +0700

    xsxxs

--- noitu.py
+++ noitu.py
@@ -1,10 +1,18 @@
-import os, discord
+import os
+import discord
+
 from discord.ext import commands
 
 # Khởi tạo intents
 intents = discord.Intents.default()
 intents.message_content = True
 
+# Lazy import aiohttp
+def get_aiohttp_client():
+    import aiohttp
+    return aiohttp.ClientSession()
+
+    
 # Tạo một instance bot
 bot = commands.Bot(command_prefix='!', intents=intents)
 
